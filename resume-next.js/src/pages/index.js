278204aa49ab2b80e5f1fae9362d4b869590180d
unknown <brianwagako@gmail.com>
2023-01-25 09:43:39 +0300

    aboutpage

--- resume-next.js/src/pages/index.js
+++ resume-next.js/src/pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Navbar from 'components/Navbar'
 import Main from 'components/Main'
+import About from 'components/About'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function Home() {
       <Navbar/>
 
       <Main/>
+      <About/>
       
     </>
   )
